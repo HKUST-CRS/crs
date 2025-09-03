cea7d442b627c55ebdc4593b02384dcd8cd3fc08
Flandia Yingman <me@flandia.dev>
2025-09-03 19:49:22 +0800

    feat: amazing animation

--- packages/site/app/page.tsx
+++ packages/site/app/page.tsx
@@ -1,13 +1,23 @@
 import { NavigationCard } from "@/components/navigation-card";
 import { columns } from "@/components/requests/columns";
 import { DataTable } from "@/components/requests/data-table";
+import TextType from "@/components/TextType";
 
 export default function Home() {
   return (
     <article className="max-w-4xl mx-auto my-64 flex flex-col gap-8">
       <header className="text-center">
         <h1>CRS</h1>
-        <div>CSE Request System</div>
+        <TextType
+          text="CSE Request System"
+          as="div"
+          textColors={["#000000"]}
+          cursorCharacter="_"
+          variableSpeed={{
+            min: 120,
+            max: 240,
+          }}
+        />
         <div className="text-xs text-gray-500">(Students' View)</div>
       </header>
       <section className="grid grid-cols-2 gap-4 mx-16">
