42792ebc55b12260e212fe24e6d717e2c43600d2
Flandia Yingman <me@flandia.dev>
2025-09-03 16:12:45 +0800

    chore: set up tailwind

--- packages/site/app/page.tsx
+++ packages/site/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
-	return (
-		<article className="max-w-7xl mx-auto my-64">
-			<header className="text-center">
-				<h1>CRS</h1>
-				<div>CSE Request System</div>
-			</header>
-		</article>
-	);
+  return (
+    <article className="max-w-7xl mx-auto my-64">
+      <header className="text-center">
+        <h1>CRS</h1>
+        <div>CSE Request System</div>
+      </header>
+    </article>
+  );
 }
