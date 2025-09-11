9ac96daa0eea718cc558259ec07baeaeaa1e9071
Flandia Yingman <me@flandia.dev>
2025-09-12 01:32:17 +0800

    refactor: refine models

--- packages/service/models/requests/base.ts
+++ packages/service/models/requests/base.ts
@@ -1,40 +1,41 @@
 import { z } from 'zod'
-import { User } from '../user'
-import { Course } from '../course'
-
-export const RequestType = z.enum(['Swap Section', 'Deadline Extension'])
-export type RequestType = z.infer<typeof RequestType>
+import { RequestType } from './type'
+import { UserId } from '../user'
+import { CourseId } from '../course'
 
 export const RequestDetails = z.object({
-  reason: z.string(),
-  proof: z.array(z.file().max(5 * 1024 * 1024)),
+  reason: z.string()
+    .meta({ description: 'A brief explanation of the request.' }),
+  proof: z.array(z.file().max(5 * 1024 * 1024))
+    .meta({ description: 'Optional supporting documents or files for the request.' }),
 })
 
 export const Response = z.object({
-  instructorEmail: User.shape.email,
+  from: UserId,
   timestamp: z.iso.datetime(),
   approved: z.boolean(),
   remark: z.string(),
 })
 export type Response = z.infer<typeof Response>
 
-const BaseRequest = z.object({
+export const BaseRequest = z.object({
+  from: UserId,
+  course: CourseId,
   type: RequestType,
-  studentEmail: User.shape.email,
-  courseCode: Course.shape.code,
-  courseSemester: Course.shape.semester,
-  timestamp: z.iso.datetime(),
-  metadata: z.object(),
   details: RequestDetails,
+  timestamp: z.iso.datetime(),
   response: z.union([Response, z.null()]),
 })
 
 /**
- * Helper function to create request type
+ * A constructor function to create specific request types with associated metadata.
+ * @param type The type of the request.
+ * @param metadata The metadata schema specific to the request type.
+ * @returns A Zod schema representing the complete request structure.
  */
 export const createRequestType = (
   type: RequestType,
-  metadata: z.ZodTypeAny,
+  metadata: z.ZodType,
 ) => {
   return BaseRequest.extend({
     type: z.literal(type),
--- packages/service/models/requests/swapSection.ts
+++ packages/service/models/requests/swapSection.ts
@@ -1,16 +1,23 @@
 import { z } from 'zod'
 import { createRequestType } from './base'
 
+export const SwapSectionMeta = z.object({
+  fromSection: z.string()
+    .meta({ description: 'The section code to swap from.' }),
+  fromDate: z.iso.date()
+    .meta({ description: 'The date of the section to swap from.' }),
+  toSection: z.string()
+    .meta({ description: 'The desired section code to swap to.' }),
+  toDate: z.iso.date()
+    .meta({ description: 'The date of the section to swap to.' }),
+})
+export type SwapSectionMeta = z.infer<typeof SwapSectionMeta>
+
 export const SwapSectionRequest = createRequestType(
   'Swap Section',
-  z.object({
-    fromSection: z.string().meta({ description: 'Original Section Code' }),
-    fromDate: z.iso.date().meta({ description: 'Date of the Original Class' }),
-    toSection: z.string().meta({ description: 'Desired Section Code' }),
-    toDate: z.iso.date().meta({ description: 'Date of the Desired Class' }),
-  }),
+  SwapSectionMeta,
 ).meta({
+  title: 'Swap Section',
   description: 'Request for swapping to another section for one class',
 })
-
 export type SwapSectionRequest = z.infer<typeof SwapSectionRequest>
