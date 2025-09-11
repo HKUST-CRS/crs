2a1fdcb33292e30954619f1114a8ff3a8c68cff2
Flandia Yingman <me@flandia.dev>
2025-09-12 01:32:17 +0800

    refactor: refine models

--- packages/service/models/requests/deadlineExtension.ts
+++ packages/service/models/requests/deadlineExtension.ts
@@ -1,16 +1,20 @@
 import { z } from 'zod'
 import { createRequestType } from './base'
 
+export const DeadlineExtensionMeta = z.object({
+  assignmentName: z.string()
+    .meta({ description: 'The name of the assignment.' }),
+  requestedDeadline: z.iso
+    .datetime()
+    .meta({ description: 'The proposed and requested new deadline.' }),
+})
+export type DeadlineExtensionMeta = z.infer<typeof DeadlineExtensionMeta>
+
 export const DeadlineExtensionRequest = createRequestType(
   'Deadline Extension',
-  z.object({
-    assignmentName: z.string().meta({ description: 'Name of the Assignment' }),
-    requestedDeadline: z.iso
-      .datetime()
-      .meta({ description: 'The New Deadline Being Requested' }),
-  }),
+  DeadlineExtensionMeta,
 ).meta({
+  title: 'Deadline Extension',
   description: 'Request for extension of assignment deadlines',
 })
-
 export type DeadlineExtensionRequest = z.infer<typeof DeadlineExtensionRequest>
