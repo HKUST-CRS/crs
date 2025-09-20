14046365836e4a91badb3163c519447a5ba5fcc5
tonyyuyiding <tonyyuyiding@gmail.com>
2025-09-20 16:07:31 +0800

    feat: update DbConn design

--- packages/service/db/conn.ts
+++ packages/service/db/conn.ts
@@ -20,19 +20,19 @@ export class DbConn {
     this._dbName = dbName
   }
 
-  private async _connect(): Promise<void> {
-    if (!this._client || !this._db) {
+  private async _connect(): Promise<mongoDB.Db> {
+    if (this._client && this._db) {
+      return this._db
+    }
+    else {
       this._client = new mongoDB.MongoClient(this._dbConnString)
       await this._client.connect()
-      this._db = this._client.db(this._dbName)
+      return this._client.db(this._dbName)
     }
   }
 
   async getCollections(): Promise<Readonly<Collections>> {
-    if (!this._db) {
-      await this._connect()
-    }
-    this._db = this._db! // ensured by _connect()
+    this._db ??= await this._connect()
     return {
       users: this._db.collection<User>('users'),
       courses: this._db.collection<Course>('courses'),
