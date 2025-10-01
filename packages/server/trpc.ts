ba650c4f52f62be61d4a06a3cf85f4e5ab344bb2
Flandia Yingman <me@flandia.dev>
2025-10-02 01:43:22 +0800

    feat: refine server

--- packages/server/trpc.ts
+++ packages/server/trpc.ts
@@ -4,16 +4,13 @@ const t = initTRPC.create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure.use(async (opts) => {
-  const start = Date.now();
-
   const result = await opts.next();
 
-  const durationMs = Date.now() - start;
-  const meta = { path: opts.path, type: opts.type, durationMs };
-
-  result.ok
-    ? console.log("OK request timing:", result.data, meta)
-    : console.error("Non-OK request timing", result.error, meta);
+  const meta = { path: opts.path, type: opts.type, ok: result.ok };
+  console.log("TRPC", {
+    meta,
+    ...(result.ok ? {} : { error: result.error }),
+  })
 
   return result;
 });
