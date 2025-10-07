d12fca23ee8da56341aff4e6540e2257674ac38c
Flandia Yingman <me@flandia.dev>
2025-10-07 20:52:51 +0800

    fix(auth): check id_token expiration

--- packages/site/lib/auth.ts
+++ packages/site/lib/auth.ts
@@ -1,3 +1,4 @@
+import * as jose from "jose";
 import { DateTime } from "luxon";
 import { NextResponse } from "next/server";
 import NextAuth, { type Account, type Session } from "next-auth";
@@ -14,15 +15,32 @@ function validateEmail(email: string): boolean {
 }
 
 export function validateSession(session: Session | null | undefined): boolean {
-  console.log(
-    `Checking session ${session?.user?.email}. ` +
-      `Expires at ${DateTime.fromSeconds(session?.account?.expires_at ?? 0).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)}. ` +
-      `Current time is ${DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)}. `,
-  );
-  return !!(
-    session?.account?.expires_at &&
-    Date.now() < session.account.expires_at * 1000
-  );
+  const email = session?.user?.email;
+  console.log(`[Session ${email}] Validating session...`);
+
+  const id_token = session?.account?.id_token;
+  if (!id_token) {
+    console.log(`[Session ${email}] No id_token in session.`);
+    return false;
+  }
+  try {
+    const jwt = jose.decodeJwt(id_token);
+    if (!jwt.exp) {
+      console.log(`[Session ${email}] No exp claim in id_token.`);
+      return true; // it never expires
+    }
+    const expireAt = DateTime.fromSeconds(jwt.exp);
+    const now = DateTime.now();
+    console.log(
+      `[Session ${email}] ` +
+        `Expires at ${expireAt.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)}. ` +
+        `Current time is ${now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)}. `,
+    );
+    return now < expireAt;
+  } catch (e) {
+    console.log(`[Session ${email}] Invalid id_token in session.`, e);
+    return false;
+  }
 }
 
 // https://authjs.dev/guides/integrating-third-party-backends
