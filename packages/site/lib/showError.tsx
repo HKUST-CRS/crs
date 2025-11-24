07d16fb2832b7bd0f93c07c729c0a8e03d846d83
Flandia Yingman <me@flandia.dev>
2025-11-24 16:26:00 +0800

    feat(site): properly report server error

---
+++ packages/site/lib/showError.tsx
@@ -0,0 +1,46 @@
+import { TRPCClientError } from "@trpc/client";
+import { toast } from "sonner";
+
+const ghIssue = (error: Error) => {
+  if (error instanceof TRPCClientError) {
+    const title = `[Uncaught Server TRPCError] ${error.message}`;
+    const body = `##### Meta
+\`\`\`
+${JSON.stringify(error.meta, null, 2)}
+\`\`\`
+`;
+    const params = new URLSearchParams({
+      title,
+      body,
+    });
+    return `https://www.github.com/HKUST-CRS/crs/issues/new?${params.toString()}`;
+  }
+  const title = `[Uncaught Server Error] ${error.message}`;
+  const body = `##### Meta
+\`\`\`
+${error}
+\`\`\`
+`;
+  const params = new URLSearchParams({
+    title,
+    body,
+  });
+  return `https://www.github.com/HKUST-CRS/crs/issues/new?${params.toString()}`;
+};
+
+export const showError = (error: Error) => {
+  if (error instanceof TRPCClientError) {
+    toast.error(
+      <>
+        <p>{error.message}</p>
+        <p className="text-[0.875em]">
+          Oops... An error occurs. Please{" "}
+          <a className="underline" href={ghIssue(error)} target="_blank">
+            report it
+          </a>{" "}
+          to us.
+        </p>
+      </>,
+    );
+  }
+};
