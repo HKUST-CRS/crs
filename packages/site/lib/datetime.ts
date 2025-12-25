d2d3e281c037453e4d3d0665536c82f94e398d21
Flandia Yingman <me@flandia.dev>
2025-12-26 02:48:07 +0800

    fix: docs

--- packages/site/lib/datetime.ts
+++ packages/site/lib/datetime.ts
@@ -6,15 +6,15 @@
 export const DateTimeFormatter = "LLL dd, yyyy, HH:mm 'HKT'";
 
 /**
- * The date time formatter used across the site.
+ * The date formatter used across the site.
  *
- * It is designed to be human-readable and clear so that users won't confuse the date and time.
+ * It is designed to be human-readable and clear so that users won't confuse the date.
  */
 export const DateFormatter = "LLL dd, yyyy";
 
 /**
- * The date time formatter used across the site.
+ * The time formatter used across the site.
  *
- * It is designed to be human-readable and clear so that users won't confuse the date and time.
+ * It is designed to be human-readable and clear so that users won't confuse the time.
  */
 export const TimeFormatter = "HH:mm";
