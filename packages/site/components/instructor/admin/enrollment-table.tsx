9f4527f0497a7f52cbc98e934da2b9703fd9b160
Flandia Yingman <me@flandia.dev>
2025-12-26 01:58:07 +0800

    fix: enrollment

--- packages/site/components/instructor/admin/enrollment-table.tsx
+++ packages/site/components/instructor/admin/enrollment-table.tsx
@@ -77,7 +77,7 @@ export const columns: ColumnDef<EnrollmentRow>[] = [
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Email
+          Name
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       );
