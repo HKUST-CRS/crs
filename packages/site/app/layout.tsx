d00c5eaac01ee502feb0345fcf0f3c1711cface2
Simon Wan <simonwan5577@gmail.com>
2026-01-31 03:59:10 +0800

    feat(site): add ThemeProvider from next-themes for theme auto-switching

--- packages/site/app/layout.tsx
+++ packages/site/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "sonner";
 import { TRPCReactProvider } from "@/lib/trpc-client";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,14 +26,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionProvider>
           <TRPCReactProvider>
-            {children}
-            <Toaster position="top-center" richColors />
+            <ThemeProvider 
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster position="top-center" richColors />
+            </ThemeProvider>
           </TRPCReactProvider>
         </SessionProvider>
       </body>
