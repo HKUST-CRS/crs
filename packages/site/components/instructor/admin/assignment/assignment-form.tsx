54a7b7fac30ed7c536b5818c614c57edaed5a934
w41z <simonwan5577@gmail.com>
2026-02-26 10:48:46 +0800

    fix(site): assignment configuration form logic (#108)

--- packages/site/components/instructor/admin/assignment/assignment-form.tsx
+++ packages/site/components/instructor/admin/assignment/assignment-form.tsx
@@ -4,7 +4,11 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { CalendarIcon } from "lucide-react";
 import { DateTime, Duration } from "luxon";
 import { Controller, useForm } from "react-hook-form";
-import { DateTimeFormatter } from "service/utils/datetime";
+import {
+  DateFormatter,
+  DateTimeFormatter,
+  TimeFormatter,
+} from "service/utils/datetime";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
@@ -46,6 +50,7 @@ export function AssignmentForm({
   });
 
   const due = DateTime.fromISO(form.watch("due"));
+  const dueValid = due.isValid;
 
   return (
     <form
@@ -82,32 +87,72 @@ export function AssignmentForm({
         render={({ field, fieldState }) => (
           <Field>
             <FieldLabel>Due Date</FieldLabel>
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button variant="outline">
-                  <CalendarIcon />
-                  {field.value ? (
-                    DateTime.fromISO(field.value).toFormat(DateTimeFormatter)
-                  ) : (
-                    <span>Pick a date</span>
-                  )}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0">
-                <Calendar
-                  mode="single"
-                  selected={DateTime.fromISO(field.value).toJSDate()}
-                  onSelect={(date) => {
-                    if (date) {
-                      field.onChange(
-                        DateTime.fromJSDate(date).endOf("day").toISO(),
-                      );
+            <div className="flex gap-2">
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button variant="outline" className="flex-1">
+                    <CalendarIcon />
+                    {field.value ? (
+                      DateTime.fromISO(field.value).toFormat(DateFormatter)
+                    ) : (
+                      <span>Pick a date</span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={DateTime.fromISO(field.value).toJSDate()}
+                    defaultMonth={
+                      field.value
+                        ? DateTime.fromISO(field.value).toJSDate()
+                        : DateTime.now().toJSDate()
                     }
-                  }}
-                  className="rounded-lg border shadow-sm"
-                />
-              </PopoverContent>
-            </Popover>
+                    onSelect={(date) => {
+                      if (date) {
+                        const currentValue = field.value
+                          ? DateTime.fromISO(field.value)
+                          : DateTime.now().endOf("day");
+
+                        const updated = DateTime.fromJSDate(date).set({
+                          year: date.getFullYear(),
+                          month: date.getMonth() + 1,
+                          day: date.getDate(),
+                          hour: currentValue.hour,
+                          minute: currentValue.minute,
+                          second: currentValue.second,
+                          millisecond: currentValue.millisecond,
+                        });
+                        field.onChange(updated.toISO());
+                      }
+                    }}
+                    captionLayout="dropdown"
+                    className="rounded-lg border shadow-sm"
+                  />
+                </PopoverContent>
+              </Popover>
+              <Input
+                type="time"
+                step={60 * 10}
+                disabled={!field.value}
+                value={DateTime.fromISO(field.value).toFormat(TimeFormatter)}
+                onChange={(e) => {
+                  if (field.value) {
+                    const [hour, minute] = e.target.value
+                      .split(":")
+                      .map((str) => Number(str));
+                    const updated = DateTime.fromISO(field.value).set({
+                      hour: hour,
+                      minute: minute,
+                      second: 59,
+                      millisecond: 999,
+                    });
+                    field.onChange(updated.toISO());
+                  }
+                }}
+                className="w-min"
+              />
+            </div>
             <FieldError errors={[fieldState.error]} />
           </Field>
         )}
@@ -121,9 +166,9 @@ export function AssignmentForm({
             <FieldLabel>Latest Due Date after Extension</FieldLabel>
             <Popover>
               <PopoverTrigger asChild>
-                <Button variant="outline">
+                <Button variant="outline" disabled={!dueValid}>
                   <CalendarIcon />
-                  {field.value ? (
+                  {field.value && dueValid ? (
                     due
                       .plus(Duration.fromISO(field.value))
                       .toFormat(DateTimeFormatter)
@@ -133,25 +178,38 @@ export function AssignmentForm({
                 </Button>
               </PopoverTrigger>
               <PopoverContent className="w-auto p-0">
-                {
+                {dueValid && (
                   <Calendar
                     mode="single"
                     selected={due
                       .plus(Duration.fromISO(field.value))
                       .toJSDate()}
+                    disabled={(date) =>
+                      DateTime.fromJSDate(date).startOf("day") <
+                      due.startOf("day")
+                    }
+                    defaultMonth={
+                      field.value
+                        ? due.plus(Duration.fromISO(field.value)).toJSDate()
+                        : DateTime.now().toJSDate()
+                    }
                     onSelect={(date) => {
-                      if (date) {
-                        field.onChange(
-                          DateTime.fromJSDate(date)
-                            .endOf("day")
-                            .diff(due)
-                            .toISO(),
+                      if (date && dueValid) {
+                        const extensionDateTime = DateTime.fromJSDate(date).set(
+                          {
+                            hour: due.hour,
+                            minute: due.minute,
+                            second: due.second,
+                            millisecond: due.millisecond,
+                          },
                         );
+                        field.onChange(extensionDateTime.diff(due).toISO());
                       }
                     }}
+                    captionLayout="dropdown"
                     className="rounded-lg border shadow-sm"
                   />
-                }
+                )}
               </PopoverContent>
             </Popover>
             <FieldError errors={[fieldState.error]} />
